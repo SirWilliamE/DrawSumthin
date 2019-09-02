e4385ae00dd07fc9ec1d105e24ae7b48219df447
William <William@Colachicco.com>
2019-09-02 10:43:48 -0400

    Created onCreateDialog method in LineWidthDialogFragment class.

--- app/src/main/java/colachicco/com/LineWidthDialogFragment.java
+++ app/src/main/java/colachicco/com/LineWidthDialogFragment.java
@@ -1,4 +1,49 @@
 package colachicco.com;
 
-public class LineWidthDialogFragment {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+// class for the Select Line Width dialog
+public class LineWidthDialogFragment extends DialogFragment {
+    private ImageView widthImageView;
+
+    // create an AlertDialog and return it
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        // create the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View lineWidthDialogView = getActivity().getLayoutInflater().inflate(
+                R.layout.fragment_line_width, null);
+        builder.setView(lineWidthDialogView); // add GUI to dialog
+
+        // set the AlertDialog's message
+        builder.setTitle(R.string.title_line_width_dialog);
+
+        // get the ImageView
+        widthImageView = (ImageView) lineWidthDialogView.findViewById(R.id.widthImageView);
+
+        // configure widthSeekBar
+        final DoodleView doodleView = getDoodleFragment().getDoodleView();
+        final SeekBar widthSeekBar = (SeekBar) lineWidthDialogView.findViewById(R.id.widthSeekBar);
+        widthSeekBar.setOnSeekBarChangeListener(lineWidthChanged);
+        widthSeekBar.setProgress(doodleView.getLineWidth());
+
+        // add Set Line Width Button
+        builder.setPositiveButton(R.string.button_set_line_width,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        doodleView.setLineWidth(widthSeekBar.getProgress());
+                    }
+                });
+        return builder.create(); // return dialog
+    }
+
+
 }
