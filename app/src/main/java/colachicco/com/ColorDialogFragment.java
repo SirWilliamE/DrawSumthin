5727fbc676f4ccfa9ed26cfcea5713375d2a07f9
William <William@Colachicco.com>
2019-09-01 22:06:23 -0400

    Created printImage method in DoodleView class.

--- app/src/main/java/colachicco/com/ColorDialogFragment.java
+++ app/src/main/java/colachicco/com/ColorDialogFragment.java
@@ -1,4 +1,19 @@
 package colachicco.com;
 
-public class ColorDialogFragment {
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class ColorDialogFragment extends DialogFragment {
+
+    private SeekBar alphaSeekBar;
+    private SeekBar redSeekBar;
+    private SeekBar greenSeekBar;
+    private SeekBar blueSeekBar;
+    private View colorView;
+    private int color;
+
+
+
+
 }
