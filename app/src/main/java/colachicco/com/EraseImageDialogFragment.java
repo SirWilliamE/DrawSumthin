e114c6b5a4e7e968af936a84f124759682c38fd6
William <William@Colachicco.com>
2019-09-09 16:19:50 -0400

    Created onCreateDialog method in EraseImageDialogFragment class.

--- app/src/main/java/colachicco/com/EraseImageDialogFragment.java
+++ app/src/main/java/colachicco/com/EraseImageDialogFragment.java
@@ -1,4 +1,33 @@
+// allows the user to erase the image
 package colachicco.com;
 
-public class EraseImageDialogFragment {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class EraseImageDialogFragment extends DialogFragment {
+    // create an AlertDialog and return it
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // set the AlertDialog message
+        builder.setMessage(R.string.message_erase);
+
+        // add Erase Button
+        builder.setPositiveButton(R.string.button_erase,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDoodleFragment().getDoodleView().clear(); // clear image
+                    }
+                });
+
+        // add cancel button
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create(); // return dialog
+    }
+
 }
